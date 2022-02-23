c7f34c12358fe7528011f554266352d8beebbd15
mustafa <mustafa.sak@outlook.fr>
2022-02-23 11:36:52 +0100

    [PWA] Testing online

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="header">
         <div class="banner" v-if="deferredPrompt">
-            <p>Installer la nouvelle application de legalyspace pour faciliter l'accès à vos documents !</p>
-            <template>
-            <button class="link" @click="dismiss">Dismiss</button>
-            <button class="button button-submit" @click="install">Install</button>
-            </template>
+            <p class="text__body">Installer la nouvelle application de legalyspace pour faciliter l'accès à vos documents !</p>
+            <div class="banner__link">
+                <a class="link" @click="dismiss">Dismiss</a>
+                <a class="link link-filled" @click="install">Install</a>
+            </div>
         </div>
-        <router-link to="/">
-            <img class="header__logo"
-                src="@/assets/logo.svg"
-                alt="Logo LegalySpace" />
-        </router-link>
-        <div id="header__navigation">
-            <router-link class="link" to="/login">Connexion</router-link>
+        <div class="header__inner">
+            <router-link to="/">
+                <img class="header__logo"
+                    src="@/assets/logo.svg"
+                    alt="Logo LegalySpace" />
+            </router-link>
+            <div id="header__navigation">
+                <router-link class="link" to="/login">Connexion</router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -53,11 +55,6 @@
         box-shadow: 0 0px 14px 0 rgb(0 0 0 / 10%);
         position: sticky;
         top: 0;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        align-items: center;
-        padding: 1em 1.5em;
         border-radius: 0 0 12px 12px;
     }
 
@@ -69,5 +66,24 @@
 
     .header__logo {
         width: 150px;
+    }
+
+    .header__inner {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 1.5em;
+    }
+
+    .banner {
+        padding: 1em 1.5em;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .banner  .text__body {
+        padding-bottom: 1em;
     }
 </style>
