52129630a35830d6c380cfb731de396bea21a00a
mustafa <mustafa.sak@outlook.fr>
2022-02-25 19:47:19 +0100

    [Push] Testtingls

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,15 +1,14 @@
 import firebaseConfig from "../config/firebase";
 import firebase from "firebase/compat/app";
-import { database } from "firebase/database";
+import "firebase/database";
 import { getMessaging, getToken } from "firebase/messaging";
 import Vue from "vue";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
-const db = database(firebaseApp);
+const db = firebaseApp.database;
 
 getToken(messaging, { vapidKey: "BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4" });
 
 Vue.prototype.$db = db;
-Vue.prototype.$messaging = messaging;
 export default firebaseApp;
