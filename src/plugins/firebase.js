d1bf2bd9af3a5eafb361f9f9fdbb70d4ad553e64
mustafa <mustafa.sak@outlook.fr>
2022-02-25 19:01:15 +0100

    [Push] Testtingls

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -18,27 +18,22 @@ if ("serviceWorker" in navigator) {
             messaging, {
                 vapidKey: 'BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4' 
             }).then((currentToken) => {
-            if (currentToken) {
-            // Send the token to your server and update the UI if necessary
-            // ...
-            } else {
-            // Show permission request UI
-            console.log('No registration token available. Request permission to generate one.');
-            // ...
-            }
-        }).catch((err) => {
-            console.log('An error occurred while retrieving token. ', err);
-            // ...
-        }); 
+                if (currentToken) {
+                // Send the token to your server and update the UI if necessary
+                // ...
+                } else {
+                // Show permission request UI
+                console.log('No registration token available. Request permission to generate one.');
+                // ...
+                }
+            }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+                // ...
+            }); 
         })
         .catch(function(err) {
           console.log("Service worker registration failed, error:"  , err );
       }); 
-    } else {
-            
-
-    }
-
-    
+}
 
 export default firebaseApp;
