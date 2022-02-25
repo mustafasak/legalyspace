9543e2bc6d2b79d6967abf20971e6c74fee33a60
mustafa <mustafa.sak@outlook.fr>
2022-02-25 18:14:21 +0100

    [Push] Testting

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -10,47 +10,19 @@ const messaging = getMessaging();
 
 Vue.prototype.$db = db;
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("./firebase-messaging-sw.js")
-      .then(function(registration) {
-        console.log("Registration successful, scope is:", registration.scope);
-        messaging.getToken({vapidKey: 'BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4', serviceWorkerRegistration : registration })
-          .then((currentToken) => {
-            if (currentToken) {
-              console.log('current token for client: ', currentToken);
-    
-              // Track the token -> client mapping, by sending to backend server
-              // show on the UI that permission is secured
-            } else {
-              console.log('No registration token available. Request permission to generate one.');
-    
-              // shows on the UI that permission is required 
-            }
-          }).catch((err) => {
-            console.log('An error occurred while retrieving token. ', err);
-            // catch error while creating client token
-          });  
-        })
-        .catch(function(err) {
-          console.log("Service worker registration failed, error:"  , err );
-      }); 
-} else {
-    getToken(messaging, { vapidKey: 'BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4' }).then((currentToken) => {
-        if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          // ...
-        } else {
-          // Show permission request UI
-          console.log('No registration token available. Request permission to generate one.');
-          // ...
-        }
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // ...
-      });
-      
-}
+getToken(messaging, { vapidKey: 'BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
     
 
 export default firebaseApp;
