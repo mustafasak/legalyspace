5986378beaba22dcc765d49cf4095fd556cffb0e
mustafa <mustafa.sak@outlook.fr>
2022-02-25 19:04:50 +0100

    [Push] Testtingls

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,7 +1,7 @@
 import firebaseConfig from "../config/firebase";
 import firebase from "firebase/compat/app";
 import "firebase/database";
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging } from "firebase/messaging";
 import Vue from "vue";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -11,11 +11,10 @@ const messaging = getMessaging(firebaseApp);
 Vue.prototype.$db = db;
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("./service-worker.js")
+      .register("./firebase-messaging-sw.js")
       .then(function(registration) {
         console.log("Registration successful, scope is:", registration.scope);
-        getToken(
-            messaging, {
+        messaging.getToken({
                 vapidKey: 'BN9bpGyskPt346ZCQVKJI2upTsn8jVMLsef7kB2H-9IkZEwY0piGjSBQoZWOG-ZuNWcNptGoID0Zom8Ei1RNbn4' 
             }).then((currentToken) => {
                 if (currentToken) {
