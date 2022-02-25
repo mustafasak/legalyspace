b1aaca91150ffc2424777b835b2ca2683e32095d
mustafa <mustafa.sak@outlook.fr>
2022-02-25 17:53:22 +0100

    [Push] Testting

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,30 +30,3 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./firebase-messaging-sw.js")
-    .then(function(registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-      messaging.getToken({vapidKey: 'YOUR_VAPID_KEY', serviceWorkerRegistration : registration })
-        .then((currentToken) => {
-          if (currentToken) {
-            console.log('current token for client: ', currentToken);
-  
-            // Track the token -> client mapping, by sending to backend server
-            // show on the UI that permission is secured
-          } else {
-            console.log('No registration token available. Request permission to generate one.');
-  
-            // shows on the UI that permission is required 
-          }
-        }).catch((err) => {
-          console.log('An error occurred while retrieving token. ', err);
-          // catch error while creating client token
-        });  
-      })
-      .catch(function(err) {
-        console.log("Service worker registration failed, error:"  , err );
-    }); 
-  }
